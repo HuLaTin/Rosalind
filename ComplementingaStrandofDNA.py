432b3a33117a09b98c1345e8e313ec82ab839ec7
HuLaTin <hulatin@gmail.com>
2022-03-25 17:25:40 -0500

    update

---
+++ ComplementingaStrandofDNA.py
@@ -0,0 +1,31 @@
+'''
+Problem
+In DNA strings, symbols 'A' and 'T' are complements of each other, as are 'C' and 'G'.
+
+The reverse complement of a DNA string s is the string sc formed by reversing the symbols of s, then taking the complement of each symbol (e.g., the reverse complement of "GTCA" is "TGAC").
+
+Given: A DNA string s of length at most 1000 bp.
+
+Return: The reverse complement sc of s.
+'''
+
+#dna = 'AAAACCCGGT'
+
+file = open(r'Data\rosalind_revc.txt') #open from file
+dna = file.read()
+
+revComp = dna
+
+# Complement 'A' and 'T'
+revComp = revComp.replace('A', 'B')
+revComp = revComp.replace('T', 'A')
+revComp = revComp.replace('B', 'T')
+
+# Complement 'C' and 'G'
+revComp = revComp.replace('C', 'D')
+revComp = revComp.replace('G', 'C')
+revComp = revComp.replace('D', 'G')
+
+revComp = revComp[::-1] # reverse order of string
+
+print(revComp) # print
---
+++ CountingDNANucleotides.py
@@ -0,0 +1,26 @@
+'''
+Problem
+A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length of a string is the number of symbols that it contains.
+
+An example of a length 21 DNA string (whose alphabet contains the symbols 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG."
+
+Given: A DNA string s of length at most 1000 nt.
+
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+'''
+
+#dna = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC' # DNA string
+file = open(r'Data\rosalind_dna.txt') # open DNA string from file
+dna = file.read()
+
+'''
+# Or a little bit prettier
+A = dna.count('A')
+C = dna.count('C')
+G = dna.count('G')
+T = dna.count('T')
+
+print(int(A), ' ', int(C), ' ', int(G), ' ', int(T))
+'''
+
+print(int(dna.count('A')), ' ', int(dna.count('C')), ' ', int(dna.count('G')), ' ', int(dna.count('T'))) # print counts
